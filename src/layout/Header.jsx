ef5720e433848901c150fdb270731f7313133276
Jessica Patricio <jessicacacau@gmail.com>
2024-06-28 10:19:47 -0300

    add link to header title

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,7 +2,9 @@ export default function Header({ title }) {
   return (
     <header className="w-screen bg-slate-300">
       <div className="container flex flex-col mx-auto px-4 py-2">
-        <h1 className="text-2xl">{title}</h1>
+        <a href="/" alt={title} className="w-fit">
+          <h1 className="text-2xl">{title}</h1>
+        </a>
       </div>
     </header>
   );
