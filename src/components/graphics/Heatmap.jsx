006fc0350f216c46c1d3e8b84eb7cb4103a61866
Jessica Patricio <jessicacacau@gmail.com>
2024-07-01 15:00:59 -0300

    add to-do comments

--- src/components/graphics/Heatmap.jsx
+++ src/components/graphics/Heatmap.jsx
@@ -1,5 +1,9 @@
+import { countryList } from "../../data";
+
 // HeatMap de Ocorrência dos IPs nos países: Mostra um mapa de calor da ocorrência dos IPs nos países. No final, pergunta se deseja exportar o arquivo excel com os dados do gráfico.
 // inputs: nenhum
-export default function Heatmap() {
+// geographic heatmap
+// recebe dados da planilha, agrupa por país e conta a quantidade de ocorrências
+export default function Heatmap({ data }) {
   return <></>;
 }
