e49cba8b9c3f7f4fea85a40ed80ead4353afbd2b
Jessica Patricio <jessicacacau@gmail.com>
2024-07-01 13:53:01 -0300

    add documentation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,27 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Select from "./Select";
 
+/**
+ * Renders a form with inputs used to generate the graphic.
+ * @param {function} formSelectFeature Function to share the feature state with Graphic through Main.
+ * @param {function} formSelectPeriod Function to set the period state with Graphic through Main.
+ * @param {function} formSelectIp Function to set the ip state with Graphic through Main.
+ * @param {function} formSelectNumIps Function to set the numIps state with Graphic through Main.
+ * @returns {ReactNode}
+ */
 export default function Form({
   formSelectFeature,
   formSelectPeriod,
   formSelectIp,
   formSelectNumIps,
 }) {
+  // States
   const [feature, setFeature] = useState(null);
+  const [period, setPeriod] = useState(null);
+  const [ip, setIp] = useState(null);
+  const [numIps, setNumIps] = useState(null);
+
+  // Functions that will be passed to the children components to set the variables states
   function childSetFeature(feature) {
     setPeriod(null);
     setIp(null);
@@ -15,21 +29,26 @@ export default function Form({
     setFeature(feature);
   }
 
-  const [period, setPeriod] = useState(null);
   function childSetPeriod(period) {
     setPeriod(period);
   }
 
-  const [ip, setIp] = useState(null);
   function childSetIp(ip) {
     setIp(ip);
   }
 
-  const [numIps, setNumIps] = useState(null);
+  // Handlers
+  /**
+   * Sets the number of IPs to be shown in the graphic.
+   * @param {Event} event
+   */
   function handleNumIpsChange(event) {
     setNumIps(event.target.value);
   }
 
+  /**
+   * Submits the form. Shares the variables states through the props functions.
+   */
   function handleButtonClick(event) {
     formSelectFeature(feature);
     formSelectPeriod(period);
@@ -37,6 +56,9 @@ export default function Form({
     formSelectNumIps(numIps);
   }
 
+  /**
+   * Resets the form. Sets all states to null and the select to the default value.
+   */
   function handleResetClick(event) {
     setFeature(null);
     formSelectFeature(null);
@@ -50,15 +72,20 @@ export default function Form({
     formSelectNumIps(null);
   }
 
+  // Select "feature" always shows
+  // Select "period" shows if "feature" is not null
+  // Select "ip" shows if the feature is "Gráficos de Comportamento"
+  // Input "input-num-ips" shows if the feature is "Score Average Mobat dos IPs com maior variação"
+  // Buttons "submit" and "reset" show if "feature" is not null
   return (
     <div className="w-full lg:w-1/3 lg:h-full p-4 bg-slate-100 gap-4 flex items-center flex-col justify-center">
       <form className="flex flex-col gap-4 w-full items-center">
-        <Select name="feature" childSelect={childSetFeature} />
+        <Select name="feature" childSet={childSetFeature} />
 
-        {feature ? <Select name="period" childSelect={childSetPeriod} /> : null}
+        {feature ? <Select name="period" childSet={childSetPeriod} /> : null}
 
         {feature === "Gráficos de Comportamento" ? (
-          <Select name="ip" childSelect={childSetIp} />
+          <Select name="ip" childSet={childSetIp} />
         ) : null}
 
         {feature === "Score Average Mobat dos IPs com maior variação" ? (
--- src/components/Graphic.jsx
+++ src/components/Graphic.jsx
@@ -1,76 +1,21 @@
-import { mean } from "mathjs";
 import { useEffect, useState } from "react";
 import * as XLSX from "xlsx";
 import { allowedColumns } from "../data";
 
 // df_selected = trimestre escolhido
-
 // ips = df_selected['IP'].apply(extract_ip).unique()
 
-// 1. Gráficos de Comportamento
-// plot_ip_data(df_selected, ip_escolhido, mean_values)
-// input: selecionar ip de lista
-// Gráficos de Comportamento: Visualiza o comportamento de IPs específicos. De acordo com a lista de IP, o usuário escolhe qual IP deseja visualizar e gerar os gráficos de comportamento. No final, pergunta se deseja exportar a tabela excel com os dados do IP específico.
-
-// 2. Mapeamento das features
-// plot_feature_mapping(df_selected)
-// Mapeamento das Features: Mostra um mapeamento de todas as características presentes nos dados. Os gráficos gerados mostram no máximo os cinco valores mais recorrentes de cada característica. No final, pergunta se deseja exportar um arquivo excel com as características de todos os IPs mapeadas e quantificadas do período selecionado no Menu Principal.
-
-// 3. Clusters
-// plot_clusters(df_selected)
-// Clusters: Visualização dos clusters identificados nos dados. É disponibilizada uma lista de características de reputação para selecionar  qual deseja visualizar os clusters. No final, pergunta se deseja exportar a tabela excel dos dados de todos clusters da característica da tabela selecionada no Menu Principal. Caso não queira mais visualizar, é possível voltar ao Menu Principal.
-
-// 4. Seleção de Características
-// plot_feature_selection(df_selected, allowed_columns)
-// Seleção de Características: Visualiza a seleção de características feita dos dados.
-
-// 5. Importâncias para Machine Learning
-// plot_feature_importance(df_selected, allowed_columns)
-// Importâncias para Machine Learning: Exibe a importância das características para modelos de machine learning.
-
-// 6. Score Average Mobat dos IPs com maior variação
-// plot_top_ips_score_average(df_selected, num_ips)
-// input: quantidade de ips a serem exibidos
-// Variação do Score Average Mobat dos IPs com maior ocorrência: Mostra o Score Average Mobat dos IPs com maior ocorrência. Pergunta quantos IPs deseja visualizar no gráfico. No final, pergunta se deseja exportar a tabela excel com os dados do gráfico.
-
-// 7. Reputação por País
-// plot_country_score_average(df_selected)
-// Reputação por País: Visualiza o gráfico da reputação dos IPs por país.
-
-// 8. Upload da Tabela dos Ips do período
-// download_all_ip_data(df_selected)
-// Upload da Tabela dos IPs do período: Permite exportar os dados dos IPs do período selecionado.
-
-// 9. HeatMap de Ocorrência dos Ips nos países
-// plot_country_heatmap(df_selected)
-// HeatMap de Ocorrência dos IPs nos países: Mostra um mapa de calor da ocorrência dos IPs nos países. No final, pergunta se deseja exportar o arquivo excel com os dados do gráfico.
-
-// 10. Tabela de Acurácia e Tempo de Treinamento dos Modelos
-// plot_show_results_table(df_selected, allowed_columns)
-// Tabela de Acurácia e Tempo de Treinamento dos Modelos: Exibe uma tabela com a acurácia e tempo de treinamento dos modelos. Permite exportar o arquivo excel da tabela.
-
-// 11. Gráfico de Dispersão
-// plot_dispersion_ip_reports(df_selected, treat_all_as_string=True)
-// Gráfico de Dispersão: Mostra um gráfico de dispersão de todas as características dos IPs. Caso não queira mais visualizar, é possível voltar ao Menu Principal.
-
-export default function Graphic({ visualization, period, ip, numIps }) {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("./PrimeiroSemestre.xlsx")
-      .then((response) => response.arrayBuffer())
-      .then((data) => {
-        let workbook = XLSX.read(new Uint8Array(data), {
-          type: "array",
-        });
-        const sheetName = workbook.SheetNames[0];
-        const sheet = workbook.Sheets[sheetName];
-        const sheetData = XLSX.utils.sheet_to_json(sheet);
-        setData(sheetData);
-      });
-  }, []);
-
-  // quantidade, soma, média
-  let meanValues = {
+/**
+ * Renders a graphic based on the inputs from the form.
+ * @param {function} feature Function to share the feature state with Form through Main.
+ * @param {function} period Function to share the period state with Form through Main.
+ * @param {function} ip Function to share the ip state with Form through Main.
+ * @param {function} numIps Function to share the numIps state with Form through Main.
+ * @returns {ReactNode}
+ */
+export default function Graphic({ feature, period, ip, numIps }) {
+  // Null state of meanValues
+  const nullMeanValues = {
     abuseipdb_confidence_score: { num: 0, sum: 0, mean: null },
     abuseipdb_total_reports: { num: 0, sum: 0, mean: null },
     abuseipdb_num_distinct_users: { num: 0, sum: 0, mean: null },
@@ -85,78 +30,148 @@ export default function Graphic({ visualization, period, ip, numIps }) {
     score_average_Mobat: { num: 0, sum: 0, mean: null },
   };
 
-  if (data) {
-    data.forEach((row) => {
-      if (row.abuseipdb_confidence_score) {
-        meanValues.abuseipdb_confidence_score.num += 1;
-        meanValues.abuseipdb_confidence_score.sum += Number(
-          row.abuseipdb_confidence_score
-        );
-      }
-      if (row.abuseipdb_total_reports) {
-        meanValues.abuseipdb_total_reports.num += 1;
-        meanValues.abuseipdb_total_reports.sum += Number(
-          row.abuseipdb_total_reports
-        );
-      }
-      if (row.abuseipdb_num_distinct_users) {
-        meanValues.abuseipdb_num_distinct_users.num += 1;
-        meanValues.abuseipdb_num_distinct_users.sum += Number(
-          row.abuseipdb_num_distinct_users
-        );
-      }
-      if (row.virustotal_reputation) {
-        meanValues.virustotal_reputation.num += 1;
-        meanValues.virustotal_reputation.sum += Number(
-          row.virustotal_reputation
-        );
-      }
-      if (row.harmless) {
-        meanValues.harmless.num += 1;
-        meanValues.harmless.sum += Number(row.harmless);
-      }
-      if (row.malicious) {
-        meanValues.malicious.num += 1;
-        meanValues.malicious.sum += Number(row.malicious);
-      }
-      if (row.suspicious) {
-        meanValues.suspicious.num += 1;
-        meanValues.suspicious.sum += Number(row.suspicious);
-      }
-      if (row.undetected) {
-        meanValues.undetected.num += 1;
-        meanValues.undetected.sum += Number(row.undetected);
-      }
-      if (row.IBM_score) {
-        meanValues.IBM_score.num += 1;
-        meanValues.IBM_score.sum += Number(row.IBM_score);
-      }
-      if (row["IBM_average history Score"]) {
-        meanValues.IBM_average_history_Score.num += 1;
-        meanValues.IBM_average_history_Score.sum += Number(
-          row["IBM_average history Score"]
-        );
-      }
-      if (row["IBM_most common score"]) {
-        meanValues.IBM_most_common_score.num += 1;
-        meanValues.IBM_most_common_score.sum += Number(
-          row["IBM_most common score"]
-        );
-      }
-      if (row.score_average_Mobat) {
-        meanValues.score_average_Mobat.num += 1;
-        meanValues.score_average_Mobat.sum += Number(row.score_average_Mobat);
-      }
-    });
+  // States
+  const [url, setUrl] = useState(getFileName(period));
+  const [meanValues, setMeanValues] = useState(nullMeanValues);
+  const [data, setData] = useState([]);
 
-    Object.keys(meanValues).forEach((key) => {
-      meanValues[key].mean = meanValues[key].sum / meanValues[key].num;
-    });
+  // Updates the url with the filename for the respective period when it changes.
+  useEffect(() => {
+    setUrl(getFileName(period));
+  }, [period]);
+
+  /**
+   * Matches the period with the respective filename.
+   * @param {string} period The period of the data from the file.
+   * @returns {string} The filename.
+   */
+  function getFileName(period) {
+    switch (period) {
+      case "jan-mar/2023":
+        return "PrimeiroSemestre.xlsx";
+      case "abr-jun/2023":
+        return "SegundoSemestre.xlsx";
+      case "jul-set/2023":
+        return "TerceiroSemestre.xlsx";
+      case "out-dez/2023":
+        return "TerceiroSemestre.xlsx";
+      default:
+        return "SegundoSemestre.xlsx";
+    }
   }
 
+  // Fetches the data from the new url when the period changes and set the mean values to null, since they need to be recalculated.
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.arrayBuffer())
+      .then((data) => {
+        let workbook = XLSX.read(new Uint8Array(data), {
+          type: "array",
+        });
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+        const sheetData = XLSX.utils.sheet_to_json(sheet);
+        setData(sheetData);
+      })
+      .catch((error) => console.error(error));
+    setMeanValues(nullMeanValues);
+  }, [period]);
+
+  // Calculates the mean values when the data changes. Runs through the data rows twice.
+  useEffect(() => {
+    if (data) {
+      // First run to calculate the sum of the values
+      data.forEach((row) => {
+        let copyMeanValues = { ...meanValues };
+
+        if (row.abuseipdb_confidence_score) {
+          copyMeanValues.abuseipdb_confidence_score.num += 1;
+          copyMeanValues.abuseipdb_confidence_score.sum += Number(
+            row.abuseipdb_confidence_score
+          );
+        }
+        if (row.abuseipdb_total_reports) {
+          copyMeanValues.abuseipdb_total_reports.num += 1;
+          copyMeanValues.abuseipdb_total_reports.sum += Number(
+            row.abuseipdb_total_reports
+          );
+        }
+        if (row.abuseipdb_num_distinct_users) {
+          copyMeanValues.abuseipdb_num_distinct_users.num += 1;
+          copyMeanValues.abuseipdb_num_distinct_users.sum += Number(
+            row.abuseipdb_num_distinct_users
+          );
+        }
+        if (row.virustotal_reputation) {
+          copyMeanValues.virustotal_reputation.num += 1;
+          copyMeanValues.virustotal_reputation.sum += Number(
+            row.virustotal_reputation
+          );
+        }
+        if (row.harmless) {
+          copyMeanValues.harmless.num += 1;
+          copyMeanValues.harmless.sum += Number(row.harmless);
+        }
+        if (row.malicious) {
+          copyMeanValues.malicious.num += 1;
+          copyMeanValues.malicious.sum += Number(row.malicious);
+        }
+        if (row.suspicious) {
+          copyMeanValues.suspicious.num += 1;
+          copyMeanValues.suspicious.sum += Number(row.suspicious);
+        }
+        if (row.undetected) {
+          copyMeanValues.undetected.num += 1;
+          copyMeanValues.undetected.sum += Number(row.undetected);
+        }
+        if (row.IBM_score) {
+          copyMeanValues.IBM_score.num += 1;
+          copyMeanValues.IBM_score.sum += Number(row.IBM_score);
+        }
+        if (row["IBM_average history Score"]) {
+          copyMeanValues.IBM_average_history_Score.num += 1;
+          copyMeanValues.IBM_average_history_Score.sum += Number(
+            row["IBM_average history Score"]
+          );
+        }
+        if (row["IBM_most common score"]) {
+          copyMeanValues.IBM_most_common_score.num += 1;
+          copyMeanValues.IBM_most_common_score.sum += Number(
+            row["IBM_most common score"]
+          );
+        }
+        if (row.score_average_Mobat) {
+          copyMeanValues.score_average_Mobat.num += 1;
+          copyMeanValues.score_average_Mobat.sum += Number(
+            row.score_average_Mobat
+          );
+        }
+        setMeanValues(copyMeanValues);
+      });
+
+      // Second run to calculate the mean values
+      Object.keys(meanValues).forEach((key) => {
+        let copyMeanValues = { ...meanValues };
+        copyMeanValues[key].mean =
+          copyMeanValues[key].sum / copyMeanValues[key].num;
+        setMeanValues(copyMeanValues);
+      });
+    }
+  }, [data]);
+
   return (
-    <div className="w-full lg:w-2/3 lg:h-full flex items-center justify-center p-4 flex-grow bg-slate-100">
+    <div className="w-full lg:w-2/3 lg:h-full flex items-center justify-center p-4 flex-grow bg-slate-100 flex-col">
       <div>Gráfico</div>
+      {/* <div>
+        {data
+          ? Object.keys(meanValues).map((value, key) => (
+              <p key={key}>
+                {value}, {meanValues[value].num},{meanValues[value].sum},{" "}
+                {meanValues[value].mean}
+              </p>
+            ))
+          : null}
+      </div> */}
     </div>
   );
 }
