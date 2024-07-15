181d8ba25b009d7f2bed800f5a63af6121df4bbb
Jessica Patricio <jessicacacau@gmail.com>
2024-07-15 13:34:20 -0300

    add plot to score average

--- src/features/ScoreAverage.jsx
+++ src/features/ScoreAverage.jsx
@@ -1,5 +1,11 @@
-// Variação do Score Average Mobat dos IPs com maior ocorrência: Mostra o Score Average Mobat dos IPs com maior ocorrência. Pergunta quantos IPs deseja visualizar no gráfico. No final, pergunta se deseja exportar a tabela excel com os dados do gráfico.
-// inputs: numero de ips (min 1), top (value 0.92, step 0.01, min 0, max 1), bottom (value 0.3, step 0.01, min 0, max 1)
-export default function ScoreAverage({ data, period, numIps }) {
-  return <>Score Average</>;
+import { useEffect, useState } from "react";
+import api from "../services/api";
+import Plot from "react-plotly.js";
+
+export default function ScoreAverage({ numIps }) {
+  return (
+    <>
+      <Plot />
+    </>
+  );
 }
