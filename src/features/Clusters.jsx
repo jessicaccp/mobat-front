b3e8943af188f7525fa4469776a33e3ad5708916
Jessica Patricio <jessicacacau@gmail.com>
2024-07-15 11:11:18 -0300

    add number of clusters

--- src/features/Clusters.jsx
+++ src/features/Clusters.jsx
@@ -1,27 +1,32 @@
 import { useEffect } from "react";
+import Plot from "react-plotly.js";
 import api from "../services/api";
 
 export default function Clusters({ columnCluster, numClusters }) {
   const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    if (columnCluster) {
-      setIsLoading(true);
+    if (columnCluster && numClusters) {
+      setLoading(true);
       fetch(api)
         .then((response) => {
           if (response.ok) return response.json();
           throw new Error(`Failed to fetch data: ${response}`);
         })
         .then((data) => setData(data))
-        .then(() => setIsLoading(false))
+        .then(() => setLoading(false))
         .catch((error) => setError(error));
     }
-  }, [columnCluster]);
+  }, [columnCluster, numClusters]);
 
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error || error.message}</p>;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error?.message || error}</p>;
 
-  return <>Clusters</>;
+  return (
+    <>
+      <Plot />
+    </>
+  );
 }
