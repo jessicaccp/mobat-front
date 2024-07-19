371a970570510b7a81dd0840898ed727e7379571
Jessica Patricio <jessicacacau@gmail.com>
2024-07-19 10:21:45 -0300

    config absolute paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      src: "/src",
+      assets: "/src/assets",
+      components: "/src/components",
+      features: "/src/features",
+      layout: "/src/layout",
+      routes: "/src/routes",
+      services: "/src/services",
+      tests: "/src/tests",
+    },
+  },
+});
