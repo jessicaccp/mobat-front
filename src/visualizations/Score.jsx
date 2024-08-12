73a945f8405e185191e0d8f82cd278c05049093b
Jessica Patricio <jessicacacau@gmail.com>
2024-08-12 14:28:22 -0300

    add api data to score plot

--- src/visualizations/Score.jsx
+++ src/visualizations/Score.jsx
@@ -6,51 +6,70 @@ import Error from "layout/Error";
 
 const Score = () => {
   const numIps = useFormStore((state) => state.score.num);
+  const year = useFormStore((state) => state.year);
   const errorMessage = "Number of IPs not selected";
 
+  const [url, setUrl] = useState(null);
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  // --- TEST
   useEffect(() => {
-    setData();
-  }, []);
+    setUrl(`top-ips-score-average/?num_ips=${numIps}&year=${year}&view=json`);
+  }, [numIps, year]);
 
-  // --- REAL
-  // useEffect(() => {
-  //   if (numIps) {
-  //     setLoading(true);
-  //     fetch(api)
-  //       .then((response) => {
-  //         if (response.ok) return response.json();
-  //         throw new Error(`Failed to fetch data: ${response}`);
-  //       })
-  //       .then((data) => setData(data))
-  //       .then(() => setLoading(false))
-  //       .catch((error) => setError(error));
-  //   }
-  // }, [numIps]);
+  useEffect(() => {
+    if (url && numIps && year) {
+      setLoading(true);
+      api
+        .get(url)
+        .then((response) => {
+          if (response.status === 200) return response.data;
+          throw new Error(
+            `Failed to fetch data: ${response.status} ${response.statusText}`
+          );
+        })
+        .then((data) => setData(data))
+        .then(() => setLoading(false))
+        .catch((error) => setError(error));
+    }
+  }, [url]);
 
   if (!numIps) return <Error message={errorMessage} />;
   if (loading) return <p>Loading...</p>;
   if (error) return <Error message={error?.message || error} />;
-  // if (!data) return <Error message="No data" />;
+  if (!data) return <Error message="No data" />;
 
   return (
     <>
       <Plot
         divId="chart"
-        data={[{}]}
+        data={
+          data
+            ? data.top_ips_data.map((item, key) => ({
+                x: [key + 1, key + 1],
+                y: [item.MinScore.toFixed(2), item.MaxScore.toFixed(2)],
+                type: "scatter",
+                mode: "lines+markers",
+                name: item.IP,
+                text: `Score variation: ${item.ScoreVariation.toFixed(2)}`,
+              }))
+            : null
+        }
         layout={{
           autosize: true,
           title: "Score Average Mobat dos IPs com maior variação",
-          xaxis: { title: "" },
-          yaxis: { title: "" },
+          modebar: { orientation: "v", remove: ["lasso", "select"] },
+          xaxis: { title: "IPs", automargin: true },
+          yaxis: { title: "MoBAt average score", automargin: true },
         }}
-        config={{ locale: "en-us" }}
+        config={{
+          locale: "en-us",
+          scrollZoom: true,
+          displaylogo: false,
+          responsive: true,
+        }}
         useResizeHandler
-        responsive
         className="w-full h-full"
       />
     </>
