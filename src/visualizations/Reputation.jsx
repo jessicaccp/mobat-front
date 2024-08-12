a08c86942db753e4c46b544174d77e0db9bf150e
Jessica Patricio <jessicacacau@gmail.com>
2024-08-12 14:34:45 -0300

    add data fetch

--- src/visualizations/Reputation.jsx
+++ src/visualizations/Reputation.jsx
@@ -7,63 +7,41 @@ import Error from "layout/Error";
 
 const Reputation = () => {
   const country = useFormStore((state) => state.reputation.country);
+  const year = useFormStore((state) => state.year);
   const errorMessage = "Country not selected";
 
+  const [url, setUrl] = useState(null);
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  // --- TEST
   useEffect(() => {
-    setData();
-  }, []);
-
-  // --- REAL
-  // useEffect(() => {
-  //   if (country) {
-  //     setLoading(true);
-  //     fetch(api)
-  //       .then((response) => {
-  //         if (response.ok) return response.json();
-  //         throw new Error(`Failed to fetch data: ${response}`);
-  //       })
-  //       .then((data) => setData(data))
-  //       .then(() => setLoading(false))
-  //       .catch((error) => setError(error));
-  //   }
-  // }, [country]);
-
-  const [score, setScore] = useState(null);
-  const [mean, setMean] = useState(null);
-  const [countryCode, setCountryCode] = useState(null);
-  const scores = [];
-  let sum = 0;
+    setUrl(
+      `country-score-average/?year=${year}&country=${country}&metric=average&view=json`
+    );
+  }, [country, year]);
 
   useEffect(() => {
-    if (data && countryCode) {
-      sum = 0;
-      data.map((item) => {
-        if (item.abuseipdb_country_code === countryCode) {
-          scores.push(Number(item.score_average_Mobat));
-          sum += Number(item.score_average_Mobat);
-        }
-      });
-      setScore(scores);
-      setMean(sum / (scores.length || 1));
+    if (url && country && year) {
+      setLoading(true);
+      api
+        .get(url)
+        .then((response) => {
+          if (response.status === 200) return response.data;
+          throw new Error(
+            `Failed to fetch data: ${response.status} ${response.statusText}`
+          );
+        })
+        .then((data) => setData(data))
+        .then(() => setLoading(false))
+        .catch((error) => setError(error));
     }
-  }, [data]);
-
-  useEffect(() => {
-    if (country) {
-      let alpha2 = iso31661.filter((item) => item.name === country);
-      // if (alpha2) setCountryCode(alpha2);
-    }
-  }, [country]);
+  }, [url]);
 
   if (!country) return <Error message={errorMessage} />;
   if (loading) return <p>Loading...</p>;
   if (error) return <Error message={error?.message || error} />;
-  // if (!data) return <Error message="No data" />;
+  if (!data) return <Error message="No data" />;
 
   return (
     <>
