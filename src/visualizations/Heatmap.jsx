6c1e636dac17c29dd2533c2975c4237457c10815
Jessica Patricio <jessicacacau@gmail.com>
2024-08-12 12:58:33 -0300

    add api data to heatmap plot

--- src/visualizations/Heatmap.jsx
+++ src/visualizations/Heatmap.jsx
@@ -6,42 +6,50 @@ import api from "services/api";
 import Error from "layout/Error";
 
 const Heatmap = () => {
+  const year = useFormStore((state) => state.year);
+
+  const [count, setCount] = useState(null);
+  const [url, setUrl] = useState(null);
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  // --- TEST
   useEffect(() => {
-    setData();
-  }, []);
+    setUrl(
+      `country-score-average/?year=${year}&country=Todos&metric=count&view=json`
+    );
+  }, [year]);
 
-  // --- REAL
-  // useEffect(() => {
-  //   setLoading(true);
-  //   fetch(api)
-  //     .then((response) => {
-  //       if (response.ok) return response.json();
-  //       throw new Error(`Failed to fetch data: ${response}`);
-  //     })
-  //     .then((data) => setData(data))
-  //     .then(() => setLoading(false))
-  //     .catch((error) => setError(error));
-  // }, []);
-
-  const [countries, setCountries] = useState(null);
-  const countryCounts = {};
+  useEffect(() => {
+    if (url && year) {
+      setLoading(true);
+      api
+        .get(url)
+        .then((response) => {
+          if (response.status === 200) return response.data;
+          throw new Error(
+            `Failed to fetch data: ${response.status} ${response.statusText}`
+          );
+        })
+        .then((data) => setData(data))
+        .then(() => setLoading(false))
+        .catch((error) => setError(error));
+    }
+  }, [url]);
 
   useEffect(() => {
     if (data) {
-      for (const country of data.map((item) => item.abuseipdb_country_code)) {
-        countryCounts[iso31661Alpha2ToAlpha3[country]] = countryCounts[
-          iso31661Alpha2ToAlpha3[country]
-        ]
-          ? countryCounts[iso31661Alpha2ToAlpha3[country]] + 1
-          : 1;
-      }
-      if (countryCounts["undefined"]) delete countryCounts["undefined"];
-      setCountries(countryCounts);
+      const countries = {};
+      Object.keys(data).forEach((code) => {
+        if (code.length === 2) {
+          countries[code] = {
+            iso3: iso31661Alpha2ToAlpha3[code],
+            name: iso31661.filter((item) => item.alpha2 === code)[0].name,
+            count: data[code],
+          };
+        }
+      });
+      setCount(countries);
     }
   }, [data]);
 
@@ -57,28 +65,38 @@ const Heatmap = () => {
           {
             type: "choropleth",
             locationmode: "ISO-3",
-            locations: Object.keys(countries),
-            z: Object.values(countries),
-            text: Object.keys(countries).map(
-              (countryISO3) =>
-                iso31661.filter((country) => country.alpha3 === countryISO3)[0]
-                  .name
-            ),
+            locations: count
+              ? Object.values(count).map((country) => country.iso3)
+              : null,
+            z: count
+              ? Object.values(count).map((country) => country.count)
+              : null,
+            text: count
+              ? Object.values(count).map((country) => country.name)
+              : null,
             autocolorscale: true,
+            colorbar: {
+              title: "Occurrence",
+            },
           },
         ]}
         layout={{
           autosize: true,
-          title: "Heatmap de Ocorrência dos IPs nos Países",
+          modebar: { orientation: "v", remove: ["lasso", "select"] },
+          title: "Heatmap of occurrence of IPs per country",
           geo: {
             projection: {
               type: "robinson",
             },
           },
         }}
-        config={{ locale: "en-us" }}
+        config={{
+          locale: "en-us",
+          scrollZoom: true,
+          displaylogo: false,
+          responsive: true,
+        }}
         useResizeHandler
-        responsive
         className="w-full h-full"
       />
     </>
