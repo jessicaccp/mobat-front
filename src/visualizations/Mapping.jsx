6acd8f2fea838199199664fc90f3b3b3e159518e
Jessica Patricio <jessicacacau@gmail.com>
2024-09-02 12:27:55 -0300

    get mapping data from api

--- src/visualizations/Mapping.jsx
+++ src/visualizations/Mapping.jsx
@@ -7,44 +7,54 @@ import Loading from "layout/Loading";
 
 const Mapping = () => {
   const columnMap = useFormStore((state) => state.feature);
-
-  // Optional input
   const year = useFormStore((state) => state.year);
   const semester = useFormStore((state) => state.semester);
+
+  // Optional input
   const month = useFormStore((state) => state.month);
   const day = useFormStore((state) => state.day);
   const ip = useFormStore((state) => state.ip);
 
   // Error messages
-  const requiredInput = columnMap;
+  const requiredInput = columnMap && year && semester;
   const missingInput = "Campos obrigatórios não preenchidos";
   const noData = "Sem dados para exibição";
   const fetchError = "Falha ao solicitar dados";
 
+  const [url, setUrl] = useState(null);
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  // --- TEST
   useEffect(() => {
-    setData();
-  }, []);
+    setUrl(
+      `mapeamento-features/?action=Map%20Feature&feature_choice=${columnMap}&year=${year}${
+        month ? `&month=${month}` : ``
+      }${day ? `&day=${day}` : ``}&semester=${semester}${
+        ip ? `&specific_ip=${ip}` : ``
+      }&view=json`
+    );
+  }, [columnMap, year, month, day, semester, ip]);
 
-  // --- REAL
-  // useEffect(() => {
-  //   if (columnMap) {
-  //     setLoading(true);
-  //     setError(null);
-  //     fetch(api)
-  //       .then((response) => {
-  //         if (response.ok) return response.json();
-  //         throw new Error(`Failed to fetch data: ${response}`);
-  //       })
-  //       .then((data) => setData(data))
-  //       .then(() => setLoading(false))
-  //       .catch((error) => setError(error));
-  //   }
-  // }, [columnMap]);
+  useEffect(() => {
+    if (url && requiredInput) {
+      setLoading(true);
+      setError(null);
+      api
+        .get(url)
+        .then((response) => setData(response.data))
+        .then((data) =>
+          data.length > 0
+            ? setData(data.toSorted((a, b) => b.count - a.count))
+            : setData(data)
+        )
+        .then(() => setLoading(false))
+        .catch((error) => {
+          setError(fetchError);
+          console.error(error);
+        });
+    }
+  }, [url]);
 
   // Handle errors
   // In case of missing user input, loading, error or no data
@@ -61,8 +71,8 @@ const Mapping = () => {
         layout={{
           autosize: true,
           title: "Mapeamento das Features",
-          xaxis: { title: "", automargin: true },
-          yaxis: { title: "", automargin: true },
+          xaxis: { title: columnMap, automargin: true },
+          yaxis: { title: "Quantidade", automargin: true },
         }}
         config={{
           locale: "pt-br",
