4b01a2855838b11db2ac0fb446662f908c9dfea5
Jessica Patricio <jessicacacau@gmail.com>
2024-09-25 13:02:03 -0300

    update scripts

--- start.sh
+++ start.sh
@@ -1,38 +1,61 @@
 #!/bin/bash
+set -eo pipefail
 
 # create logs folder if none
 if [ ! -d "logs" ]; then
+  echo -e "Creating logs folder..."
   mkdir logs
+  echo -e "Logs folder created."
 fi
 LOGS_DIR=`pwd`/logs
 
 # backend
 ## update the backend submodule to the latest version
+echo -e "Updating backend submodule..."
 if [ ! -d "mobat_tool" ]; then
-  git submodule update --init --recursive
+  git submodule update --init --recursive > $LOGS_DIR/back.out 2> $LOGS_DIR/back.err
 else
-  git submodule update --recursive --remote
+  git submodule update --recursive --remote > $LOGS_DIR/back.out 2> $LOGS_DIR/back.err
 fi
+echo -e "Backend submodule updated."
 
 ## install the dependencies using python 3.12
 cd mobat_tool
-python -m pip install --upgrade pip virtualenv
+python -m pip install --upgrade pip virtualenv > $LOGS_DIR/back.out 2> $LOGS_DIR/back.err
 if [ ! -d "venv" ]; then
-  python -m virtualenv -p python3.12 venv
+  echo -e "Creating virtual environment..."
+  python -m virtualenv -p python3.12 venv > $LOGS_DIR/back.out 2> $LOGS_DIR/back.err
+  echo -e "Virtual environment created."
 fi
-source venv/bin/activate
-python -m pip install -r requirements.txt
+source venv/bin/activate > $LOGS_DIR/back.out 2> $LOGS_DIR/back.err
+echo -e "Installing backend dependencies..."
+python -m pip install -r requirements.txt > $LOGS_DIR/back.out 2> $LOGS_DIR/back.err
+python -m pip install setuptools > $LOGS_DIR/back.out 2> $LOGS_DIR/back.err
+echo -e "Backend dependencies installed."
 
 ## run the django server
+echo -e "Starting backend server..."
 nohup python -u manage.py runserver 8000 > $LOGS_DIR/back.out 2> $LOGS_DIR/back.err &
+echo -e "\nBackend server running.\n"
+
+## update table migrations
+# python manage.py makemigrations table
+# python manage.py migrate table
+# echo "Table migrations updated."
 
 # frontend
 ## get latest changes
 cd ..
-git pull
+echo -e "Updating frontend repository..."
+git pull > $LOGS_DIR/front.out 2> $LOGS_DIR/front.err
+echo -e "Frontend repository updated."
 
 ## install the dependencies
-npm install
+echo -e "Installing frontend dependencies..."
+npm install > $LOGS_DIR/front.out 2> $LOGS_DIR/front.err
+echo -e "Frontend dependencies installed."
 
 ## run the react server
+echo -e "Starting frontend server..."
 nohup npm run dev -- --port 8001 > $LOGS_DIR/front.out 2> $LOGS_DIR/front.err &
+echo -e "\nFrontend server running.\n"
--- stop.sh
+++ stop.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+set -eo pipefail
 
 # stop both back and front processes
+echo -e "Stopping backend server..."
 kill $(lsof -i:8000 | awk '{print $2}' | tail -n +2)
-kill $(lsof -i:5173 | awk '{print $2}' | tail -n +2)
+echo -e "Stopping frontend server..."
+kill $(lsof -i:8001 | awk '{print $2}' | tail -n +2)
+echo -e "Servers stopped."
